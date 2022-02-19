dcc1fa371280d71880394b2fa5ba7dee03007b95
hash-time <90512878+Hash-time@users.noreply.github.com>
2022-02-19 17:13:27 +0200

    Add files via upload

---
+++ Игра на Python/game_stats.py
@@ -0,0 +1,16 @@
+class GameStats():
+    """Отслеживание статистики для игры"""
+
+    def __init__(self, ai_game):
+        """Инициализирует статистику."""
+        self.settings = ai_game.settings
+        self.high_score = 0
+        self.reset_stats()
+
+        # Игра запускается в неактивном состоянии
+        self.game_active = False
+
+    def reset_stats(self):
+        """Сбрасывает статистику"""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
